ebde03f322982de9f17c4f3bb2eb29fef5483437
alphaboss <alphaboss1104@gmail.com>
2023-11-24 19:56:03 +0100

    third-script(2)

--- logdiff.py
+++ logdiff.py
@@ -2,22 +2,35 @@ from datetime import datetime, timedelta
 import os
 
 def process_log_file(filename):
-    output_filename = "Result_" + os.path.basename(filename)
     time_stack = []
- 
+    has_time_difference = False
+
     with open(filename, mode="r", encoding="UTF-8") as log:
-        with open(output_filename, "w") as output_file:
-            for entry, item in enumerate(log, 1):
-                time_str = item[12:20]
-                time_stamp = datetime.strptime(time_str, '%H:%M:%S')
-                time_stack.append(time_stamp)
-                if len(time_stack) > 1:
-                    diff = time_stack[1] - time_stack[0]
-                    if diff >= timedelta(seconds=1800):
-                        output_file.write(f"{entry:02d}:~ {time_str} {diff}\n")
-                    time_stack.pop(0)
-                else:
-                    output_file.write(f"{entry:02d}:~ Start {time_str}\n")
+        for item in log:
+            time_str = item[12:20]
+            time_stamp = datetime.strptime(time_str, '%H:%M:%S')
+            time_stack.append(time_stamp)
+            if len(time_stack) > 1:
+                diff = time_stack[1] - time_stack[0]
+                if diff >= timedelta(seconds=1800):
+                    has_time_difference = True
+                    break
+                time_stack.pop(0)
+ 
+    if has_time_difference:
+        output_filename = "Result_" + os.path.basename(filename)
+        with open(filename, mode="r", encoding="UTF-8") as log:
+            with open(output_filename, "w") as output_file:
+                for entry, item in enumerate(log, 1):
+                    time_str = item[12:20]
+                    time_stack.append(datetime.strptime(time_str, '%H:%M:%S'))
+                    if len(time_stack) > 1:
+                        diff = time_stack[1] - time_stack[0]
+                        if diff >= timedelta(seconds=1800):
+                            output_file.write(f"{entry:02d}:~ {time_str} {diff}\n")
+                        time_stack.pop(0)
+                    else:
+                        output_file.write(f"{entry:02d}:~ Start {time_str}\n")
 
 
 def run():
